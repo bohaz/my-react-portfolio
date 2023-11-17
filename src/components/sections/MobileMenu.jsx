c357a7a031da9c6e97cc8210389877de86c49d87
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-11-17 11:06:21 -0300

    Add mobile menu

---
+++ src/components/sections/MobileMenu.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRectangleXmark } from '@fortawesome/free-solid-svg-icons';
+
+
+function MobileMenu() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      {/* Botón para cerrar el Drawer */}
+      <IconButton
+        sx={{ position: 'absolute', top: 0, right: 0 }}
+        onClick={toggleDrawer(false)}
+      >
+         <FontAwesomeIcon icon={faRectangleXmark} />
+      </IconButton>
+      <List>
+        {['Projects', 'About', 'Contact'].map((text) => (
+          <ListItem button key={text}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2, display: { xs: 'block', md: 'none' } }}
+        onClick={toggleDrawer(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+      <Drawer
+        anchor='left'
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+      >
+        {list()}
+      </Drawer>
+    </>
+  );
+}
+
+export default MobileMenu;
