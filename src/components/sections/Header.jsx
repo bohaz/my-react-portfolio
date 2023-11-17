901a1ba9de26fb14532611384da1e4a924b9e159
Ricardo Martínez <ricardomartinezvet@gmail.com>
2023-11-17 11:04:35 -0300

    Add header

---
+++ src/components/sections/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PortfolioAppBar from '../navbar/PortfolioAppBar'; 
+
+function Header() {
+  return (
+    <header>
+      <PortfolioAppBar />
+    </header>
+  );
+}
+
+export default Header;
